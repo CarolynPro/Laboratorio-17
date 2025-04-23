452f3cb8d46d51a905189e3f54e9d0465ec01335
CarolynPro <carol.ingabil.18@gmail.com>
2025-04-22 21:01:31 -0500

    Script

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function mostrarMensaje(){
     alert("Hola, Bienvenidos a Mi Pagina")
 }
+
+/*validacion de formulario*/
+function validarFormulario() {
+    const form = document.getElementById('contactForm')
+};
+
+form.addEventListener('submit', function(event) {
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+
+    if (nombre === '' || email === '') {
+        alert('Por favor, completa todos los campos.');
+        event.preventDefault();
+    } else {
+        alert('Â¡Formulario enviado correctamente!');
+    }
+});
